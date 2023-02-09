0f00ab9bc78ec26def8949dc231b5dd505cb3b56
jasondoss <jason.j.doss@gmail.com>
2023-02-08 18:27:29 -0800

    Edited my about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ function About() {
         </div>
         <p>I am a full stack web developer who is looking to build websites that are user friendly to consumers and indispensable to the site owners. I recently earned a certificate in full stack development from the University of Irvine, with newly developed skills in JavaScript, CSS, Html, React.js, and others. Attentive to detail, I am very skilled at finding issues in codes and designing and creating apps. I am an efficient problem-solver who looks to understand what users are looking for in a web app experience and delivering quality features.
         </p>
-        <p>Recently, I applied _____ to a project where I worked with a team of ___ developers. The project created a ________. I'm excited to hone my skills as a web developer with a team whose commitment to excellence and quality is prominent. 
+        <p>I'm excited to hone my skills as a web developer with a team whose commitment to excellence and quality is prominent. 
         </p>
       </div>
     </section>
